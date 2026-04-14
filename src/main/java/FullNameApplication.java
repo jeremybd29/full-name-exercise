fd410ead6a7f99db378e4433cc060bed594e17e8
Jeremy <jeremybaez919@gmail.com>
2026-04-13 18:00:50 -0700

    full name application

---
+++ src/main/java/FullNameApplication.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        // full name string being declared
+        String fullName = firstName;
+        System.out.println(firstName);
+
+        //user enters middle name
+        System.out. print("Enter Middle name");
+        String middleName = scanner.nextLine();
+
+        // updating string method with middle name
+        if (!middleName.isEmpty())
+            fullName += " " + middleName;
+
+        // user enter last name
+        System.out.print("Enter last name");
+        String lastName = scanner.nextLine();
+
+        fullName += " " + lastName;
+
+        System.out.print("Full name: "+ fullName);
+
+
+
+
+
+
+
+
+
+    }
+}
